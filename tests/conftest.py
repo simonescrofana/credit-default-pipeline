86e844017f7264746e100d7c81581707a84ca634
Simone Scrofana <simone.scrofana@gmail.com>
2026-06-26 16:57:57 +0200

    chore(logging): fixed logging format.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,14 +7,22 @@ used in tests, which are:
     * SQLAlchemy.
 """
 
-from logging import basicConfig
+import logging
 
 import logfire
 
 from config import settings
 from database.connection import engine
 
+LOG_FORMAT = "%(asctime)s - %(levelname)s - [%(name)s] - %(message)s"
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
 logfire.configure(token=settings.LOGFIRE_TOKEN.get_secret_value())
-basicConfig(handlers=[logfire.LogfireLoggingHandler()])
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=LOG_FORMAT,
+    datefmt=DATE_FORMAT,
+    handlers=[logging.StreamHandler(), logfire.LogfireLoggingHandler()],
+)
 
 logfire.instrument_sqlalchemy(engine=engine)
